c36f4a5b13c39e589922928a1aafd1a112bf7cc6
= <das01063@snu.ac.kr>
2020-02-11 01:53:26 +0900

    Separate the 404 page into two files

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import NotFound from '../components/notfound';
 
 const NotFoundPage: React.FC = () => (
-  <Layout>
-    <>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <Link to="/">Go back to the homepage</Link>
-    </>
-  </Layout>
+  <>
+    <SEO title="404: Not found" />
+    <Layout>
+      <NotFound />
+    </Layout>
+  </>
 );
 
 export default NotFoundPage;
